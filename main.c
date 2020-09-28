780bc62ea8924aab2e5eb237429d5248bbf44807
FrCorrea <71855263+FrCorrea@users.noreply.github.com>
2020-09-28 20:10:38 -0300

    leitura
    fiz a parte de leitura

--- main.c
+++ main.c
@@ -1,7 +1,119 @@
-aisudhfiuashdfui
-asdfiuashdfui
-asdfijasdfuj
-asdfnsadf
-sadfjaisdfjnjsdnfsa
-fsandkfjnsadf
-OPA
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Familia{
+    char nome_fam;
+    int qtd_fam;
+    int virus;
+    struct Familia *prox;
+}Fam;
+typedef struct CabFam{
+    Fam *prox;
+    Fam *ult;
+    Fam *prim;
+}Cfam;
+
+typedef struct Ligacao{
+    struct Ligacao *p1;
+    struct Ligacao *p2;
+    struct Ligacao *prox;
+}Lig;
+void anulaLig(Lig **p){
+    (*p)->p1 = NULL;
+    (*p)->p2 = NULL;
+    (*p)->prox = NULL;
+}
+void anulaFam(Cfam **p){
+    (*p)->prim = NULL;
+    (*p)->ult = NULL;
+    (*p)->prox = NULL;
+}
+
+void main(){
+    Cfam *fam = (Cfam*)malloc(sizeof(Cfam)); // LSITA FAMILIAS
+    anulaFam(&fam); //SETA NULL
+    Lig *lig = (Lig*)malloc(sizeof(Lig)); //LISTA LIGACOES
+    anulaLig(&lig); // SETA NULL
+
+    
+    int movag,vmult,mov; // VARIAVEIS A.ATUA -- V.MULTIPLICA -- MOVIMENTOS
+    int qtd; // QTD DE PESSOAS NA FAM
+    char fam1[2]; // STRING PARA NOME FAMILIA GENERICO
+    char fam2[2];//STRING PARA NOME FAMILIA 2.
+    char func[18]; //ARMAZENA STRING FUNCAO
+
+    
+    FILE *ent; // ARQUIVO ENTRADA
+    ent = fopen("entrada.txt","r");
+
+    if(ent==NULL){
+        printf("Erro na abertura do arquivo.\n");
+        exit(1);
+    }
+
+    while(!feof(ent)) //ATE O FIM DA LISTA
+    {
+        fscanf(ent,"%s",func); //LE A STRING FUNCAO
+        if(strcmp(func,"agente_atua")==0){
+            printf("%s -> ",func);
+            fscanf(ent,"%d",&movag); // GUARDA OS MOVIMENTOS APRA DEPOIS
+            printf("mov ag %d \n",movag);
+        }
+        if(strcmp(func,"vírus_multiplica")==0)//LE A STRING FUNCAO
+        {
+            printf("%s -> ",func);
+            fscanf(ent," %d ",&vmult); // GUARDA O MULTIPLICA PARA DEPOIS 
+            printf(" vmult %d \n",vmult);
+        }
+        if(strcmp(func,"inserefamilia")==0)//LE A STRING FUNCAO
+        {
+            printf("%s -> ",func); 
+            fscanf(ent,"%s",fam1); // GUARDA A FAMILIA 
+            printf("fam %s : ",fam1);
+            fscanf(ent,"%d",&qtd);
+            printf(" qtd fam %d \n",qtd);
+            //inserefamilia();
+        }
+        if(strcmp(func,"ligafamilias")==0)//LE A STRING FUNCAO
+        {   
+            printf("%s -> ",func); 
+            fscanf(ent,"%s",fam1); // GUARDA A FAMILIA  1
+            printf("fam %s : ",fam1);
+            fscanf(ent,"%s",fam2); // GUARDA A FAMILIA  2
+            printf("fam %s \n",fam2);
+            //ligafamilias();
+        }
+        if(strcmp(func,"inserevirus")==0)//LE A STRING FUNCAO
+        {
+            printf("%s -> ",func); 
+            fscanf(ent,"%s",fam1); // GUARDA A FAMILIA E JA USA 
+            printf("fam %s\n",fam1);
+            //inserevirus();
+        }
+        if(strcmp(func,"iniciasimulacao")==0)//LE A STRING FUNCAO
+        {
+            printf("%s -> ",func);
+            fscanf(ent,"%d",&mov); //GUARDA OS MOVIMENTOS
+            printf("mov %d \n",mov);
+            //iniciasimulacao() PRINCIPAL
+            // SECUNDARIAS
+            //agente_atua() USAREMOS OS MOVIMENTOS DO AGENTE AQUI (MOVAG)
+            //virus_multiplica() USAREMOS O MULTIPLICA DO VIRUS AQUI (VMULT)
+            // -----ESSAS FUNCOES SO ESTAO NO PDF DO TRABALHO
+            //agente_saude 
+            //virus_move
+        }
+        if(strcmp(func,"verificasurto")==0)//LE A STRING FUNCAO
+        {        
+            printf("%s -> ",func);
+            printf("VERIFICA SURTO \n");
+            //verificasurto();
+            break;
+            
+        }
+    }
+
+
+
+}
